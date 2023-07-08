15bcd5bc0551b3e9de40fc7f7960357ca7e8f6e2
cannugurlu <132507302+cannugurlu@users.noreply.github.com>
2023-07-08 22:44:18 +0300

    8 haziran son hal (tamamlanmadı)

--- Assets/Scripts/gunScript.cs
+++ Assets/Scripts/gunScript.cs
@@ -7,10 +7,14 @@ public class GunScript : MonoBehaviour
     [SerializeField] GameObject bulletobject;
     [SerializeField] float bulletSpeed;
     public float bulletLifeTime;
-    public float fireSpeed;
+    public float fireRate;
     public float power;
     public float minFireSpeed;
     public float minBulletLifeTime;
+
+    private void Awake()
+    {
+    }
     private void Start()
     {
         StartAtesEt();
@@ -20,19 +24,22 @@ public class GunScript : MonoBehaviour
     {
         StartCoroutine(AtesEt());
     }
-    private IEnumerator AtesEt()
+    public IEnumerator AtesEt()
     {
         while (true)
         {
-            GameObject bullet = Instantiate(bulletobject, transform.position, Quaternion.identity);
+            if (Time.timeScale > 0)
+            {
+                GameObject bullet = Instantiate(bulletobject, transform.position, Quaternion.identity);
 
-            bullet.GetComponent<Rigidbody>().velocity = Vector3.forward * bulletSpeed;
+                bullet.GetComponent<Rigidbody>().velocity = Vector3.forward * bulletSpeed;
 
-            bullet.transform.Rotate(0, 90, 0);
+                bullet.transform.Rotate(0, 90, 0);
 
-            Destroy(bullet, bulletLifeTime);
+                Destroy(bullet, bulletLifeTime);
+            }
 
-            yield return new WaitForSeconds(fireSpeed);
+            yield return new WaitForSeconds(fireRate);
         }
 
     }
--- Assets/Scripts/newGateController.cs
+++ Assets/Scripts/newGateController.cs
@@ -1,34 +1,154 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System.Drawing;
 
 public class newGateController : MonoBehaviour
 {
-    public static newGateController instance;
+    [SerializeField] int effectiveNumber;
+    public UnityEngine.Color baslangicRenk;
+    public UnityEngine.Color hedefRenk;
+    private float gecenSure = 0f;
+    private bool renkDegisti = false;
+    public float sure = 1f;
+    float transparency = 1f; 
+    float duration = 0.5f;
+    int randomVariable;
 
     private void Awake()
     {
-        instance = this;
     }
-
+    private void Start()
+    {
+        Invoke(nameof(gateCustomizer), 0.05f);
+    }
     void Update()
     {
-        if(Time.timeScale>0)
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "bullet")
         {
-            gateCustomizer();
+            Destroy(other.gameObject);
+            effectiveNumber+=10;
+            if (randomVariable == 0) // atýþ hýzý
+            {
+                if (effectiveNumber < 0)
+                {
+                    GetComponentInChildren<TextMeshPro>().text = "Atýþ Hýzý\n" + effectiveNumber.ToString();
+                }
+                else if (effectiveNumber == 0)
+                {
+                    if (!renkDegisti)
+                    {
+                        gecenSure += Time.deltaTime;
+                        float yuzdeTamamlandi = gecenSure / sure;
+                        GetComponentInChildren<Renderer>().material.color = UnityEngine.Color.Lerp(baslangicRenk, hedefRenk, sure);
+                        GetComponentInChildren<TextMeshPro>().text = "Atýþ Hýzý\n+" + effectiveNumber.ToString();
+
+                        if (gecenSure >= sure)
+                        {
+                            renkDegisti = true;
+                        }
+                    }
+                }
+                else
+                {
+                    GetComponentInChildren<TextMeshPro>().text = "Atýþ Hýzý\n+" + effectiveNumber.ToString();
+                }
+            }
+            else if (randomVariable == 1) // menzil
+            {
+                if (effectiveNumber < 0)
+                {
+                    GetComponentInChildren<TextMeshPro>().text = "Menzil\n" + effectiveNumber.ToString();
+                }
+                else if (effectiveNumber == 0)
+                {
+                    if (!renkDegisti)
+                    {
+                        gecenSure += Time.deltaTime;
+                        float yuzdeTamamlandi = gecenSure / sure;
+                        GetComponentInChildren<Renderer>().material.color = UnityEngine.Color.Lerp(baslangicRenk, hedefRenk, yuzdeTamamlandi);
+                        GetComponentInChildren<TextMeshPro>().text = "Menzil\n+" + effectiveNumber.ToString();
+
+                        if (gecenSure >= sure)
+                        {
+                            renkDegisti = true;
+                        }
+                    }
+                }
+                else
+                {
+                    GetComponentInChildren<TextMeshPro>().text = "Menzil\n+" + effectiveNumber.ToString();
+                }
+            }
+        }
+
+        if (other.gameObject.name == "car")
+        {
+            StartCoroutine(FadeOut(gameObject, transparency, duration));
+            gameObject.GetComponent<BoxCollider>().enabled = false;
         }
     }
 
     void gateCustomizer()
     {
-        int goodorbad = Random.RandomRange(0, 2);
-        if (goodorbad == 0) //gate'in pozitif özelliði olacak
+        effectiveNumber=Random.Range(1, 5)*100;
+        randomVariable = Random.Range(0, 2);
+        if (randomVariable == 0) // atýþ hýzý
         {
-
+            if (gameObject.tag == "GateGood")
+            {
+                GetComponentInChildren<TextMeshPro>().text = "Atýþ Hýzý\n +" + effectiveNumber.ToString();
+            }
+            else if (gameObject.tag == "GateBad")
+            {
+                effectiveNumber = -effectiveNumber;
+                GetComponentInChildren<TextMeshPro>().text = "Atýþ Hýzý\n " + effectiveNumber.ToString();
+            }
         }
-        else //gatein negatif özelliði olacak
+        else if(randomVariable == 1) // menzil
         {
+            if (gameObject.tag == "GateGood")
+            {
+                GetComponentInChildren<TextMeshPro>().text = "Menzil\n +" + effectiveNumber.ToString();
+            }
+            else if (gameObject.tag == "GateBad")
+            {
+                effectiveNumber = -effectiveNumber;
+                GetComponentInChildren<TextMeshPro>().text = "Menzil\n" + effectiveNumber.ToString();
+            }
+        }
+    }
 
+    IEnumerator FadeOut(GameObject parentObject, float transparency, float duration)
+    {
+        Renderer[] childRenderers = parentObject.GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer renderer in childRenderers)
+        {
+            Material material = renderer.material;
+            UnityEngine.Color color = material.color;
+
+            float elapsedTime = 0f;
+
+            while (elapsedTime < duration)
+            {
+                elapsedTime += Time.deltaTime;
+
+                // Saydamlýðý azaltarak rengi güncelle
+                color.a = Mathf.Lerp(transparency, 0f, elapsedTime / duration);
+                material.color = color;
+
+                yield return null;
+            }
+
+            // Nesneyi tamamen görünmez hale getir
+            color.a = 0f;
+            material.color = color;
         }
     }
 }
